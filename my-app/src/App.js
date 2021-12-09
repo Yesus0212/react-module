3802aec1092277b08d97e539f1960a1c988b6932
Yesus0212 <jesussoispadron@gmail.com>
2021-12-08 21:33:26 -0600

    renderizado condicional

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,25 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greeting from './components/Greeting';
 import Calculator from './components/Calculator';
+import Counter from './components/Counter/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Greeting firstName="Mariana" lastName="Lopez"/>
-        <Calculator firstVal={6} secondVal={5} operator="gato"/>
+        <img src={logo} className="App-logo" alt="logo" />        
+        <Counter />
       </header>
     </div>
   );
---
+++ my-app/src/components/Counter/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+// Importamos los styles
+import "./Counter.css";
+
+function Counter(){
+
+    const [counter, setCounter] = useState(0);
+    const [otherCounter, setOtherCounter] = useState(0);
+
+    useEffect(() => {
+        console.log("Me ejecuto cada que cambie el estado de other counter");        
+    }, [otherCounter]);
+
+    useEffect(() => {
+        console.log("Me ejecuto solo la primera vez");
+    }, []);
+
+    useEffect(() => {
+        console.log("Me ejecuto cada que cambie el estado de counter");
+        if(counter % 3 === 0 && counter > 0){
+            setOtherCounter(otherCounter + 1)
+        }
+    }, [counter]);
+
+    return (
+        <>
+            <div>
+                {/* renderizado condicional */}
+                <p className={counter % 2 === 0 ? "text-even" : "text-odd"}>Counter: {counter}</p>
+                <button className="btn" onClick={() => setCounter(counter + 1)}>Add</button>
+            </div>
+            <div>
+                <button className="btn" onClick={() => setCounter(counter - 1)}>Remove</button>
+            </div>
+        </>
+    );
+
+}
+
+export default Counter;
