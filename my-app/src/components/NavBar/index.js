3725b38561136a5cbf3c926dd3d7343c70e7b107
Yesus0212 <jesussoispadron@gmail.com>
2021-12-09 20:13:44 -0600

    Solucion de ejercicio lista de navBar, del mentor David

--- my-app/src/components/NavBar/index.js
+++ my-app/src/components/NavBar/index.js
@@ -1,44 +1,30 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 // Importamos los styles
 import "./NavBar.css";
 
 export default function NavBar(){
 
-    const [selected, setSelected] = useState(0);
+    const [active, setActive] = useState(0);
 
-    useEffect(() => {
-        if(selected === 1){
-            document.getElementById("s1").className = "selected";
-            document.getElementById("s2").className = "noSelected";
-            document.getElementById("s3").className = "noSelected";
-        }
-        else if(selected === 2){
-            document.getElementById("s2").className = "selected";
-            document.getElementById("s1").className = "noSelected";
-            document.getElementById("s3").className = "noSelected";
-        }
-        else if(selected === 3){
-            document.getElementById("s3").className = "selected";
-            document.getElementById("s2").className = "noSelected";
-            document.getElementById("s1").className = "noSelected";
-        }
-    });
+    const handleClick = (itemID) => {
+        setActive(itemID);
+    };
 
-    
+    const checkActive = (itemID) => (active === itemID ? "selected" : "");
     
     return (
         <div>
             <ul>
-                <li>
-                    <span id="s1" onClick={() => setSelected(1)}>Item 1</span>                        
+                <li className={checkActive(1)} onClick={() => handleClick(1)}>
+                    Item 1
                 </li>
-                <li>
-                    <span id="s2" onClick={() => setSelected(2)}>Item 2</span>
+                <li className={checkActive(2)} onClick={() => handleClick(2)}>
+                    Item 2
                 </li>
-                <li>
-                    <span id="s3" onClick={() => setSelected(3)}>Item 3</span>
+                <li className={checkActive(3)} onClick={() => handleClick(3)}>
+                    Item 3
                 </li>
             </ul>
         </div>
