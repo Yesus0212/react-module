ae10eb8a09fbc5cb43e87a7c835c3d1782ceb87c
Yesus0212 <jesussoispadron@gmail.com>
2021-12-09 21:29:27 -0600

    Mi solución del ejercicio de mostrar contenido en un div, dependiendo del item de la lista seleccionado

--- my-app/src/components/NavBar/index.js
+++ my-app/src/components/NavBar/index.js
@@ -4,30 +4,53 @@ import { useState } from "react";
 // Importamos los styles
 import "./NavBar.css";
 
+let content;
+
 export default function NavBar(){
 
     const [active, setActive] = useState(0);
 
-    const handleClick = (itemID) => {
+    const handleClick = (itemID) => {    
+        setContent(itemID)    
         setActive(itemID);
     };
-
+   
+    function setContent(id){
+        console.log(id)
+        switch(id){
+            case 1:
+                content = "Contenido del Item 1";
+                break;
+            case 2:
+                content = "Contenido del Item 2";
+                break;
+            case 3:
+                content = "Contenido del Item 3";
+                break;
+            default:
+                console.log("Opción invalida");
+                break;
+        }
+    }
+    
+    
     const checkActive = (itemID) => (active === itemID ? "selected" : "");
+
+    console.log(content);
     
     return (
+        <>
         <div>
             <ul>
-                <li className={checkActive(1)} onClick={() => handleClick(1)}>
-                    Item 1
-                </li>
-                <li className={checkActive(2)} onClick={() => handleClick(2)}>
-                    Item 2
-                </li>
-                <li className={checkActive(3)} onClick={() => handleClick(3)}>
-                    Item 3
-                </li>
+                <li className={checkActive(1)} onClick={() => handleClick(1)}>Item 1</li>
+                <li className={checkActive(2)} onClick={() => handleClick(2)}>Item 2</li>
+                <li className={checkActive(3)} onClick={() => handleClick(3)}>Item 3</li>
             </ul>
         </div>
+        <div>
+            <h1>{content}</h1>
+        </div>        
+        </>
     );
 
-}
+};
