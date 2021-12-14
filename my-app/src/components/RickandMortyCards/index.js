a6c0ceb2fd5a33b00c6224148bc35b67c1a9fd7d
Yesus0212 <jesussolispadron@gmail.com>
2021-12-14 16:32:30 -0600

    Ejercicio fetch API Rick and Morty con paginacion

--- my-app/src/components/RickandMortyCards/index.js
+++ my-app/src/components/RickandMortyCards/index.js
@@ -4,19 +4,23 @@ import "./RickandMortyCards.css";
 function RickandMortyCards() {
 
     const [cards, setCards] = useState([]);
+    const [page, setPage] = useState(1);
 
-    const url = "https://rickandmortyapi.com/api/character";
+    const url = "https://rickandmortyapi.com/api/character?page=";
+
 
     useEffect(() => {        
         const getInfoCards = async () => {
-            const response = await fetch(url);
+            const response = await fetch(url+page);
+            console.log(url+page)
             const data = await response.json();
             console.log(data.results);
             setCards(data.results);
         };
 
-        getInfoCards(); 
-    },[])
+        getInfoCards();
+    },[page]);
+
 
     const _cards = cards.map((card) => (
         <article key={card.id} className="characterCard">
@@ -30,16 +34,26 @@ function RickandMortyCards() {
                     </a>
                     <span className="status_icon"></span>
                     <span>
-                        {card.species} {card.status}
+                        {card.species} - {card.status}
                     </span>
-                </div>                
+                </div>
             </div>
         </article>
     ));
+
+
+    const _buttons = <div>
+                        <button className="button" onClick={() => setPage(page - 1)}>Back</button>
+                        <button className="button" onClick={() => setPage(page + 1)}>Next</button>
+                    </div>;
     
     return (
         <>
-            {_cards}
+            {_buttons}
+            <div className="contentCards">
+                {_cards}
+            </div>
+            {_buttons}
         </>
     )
 }
