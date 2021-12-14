fed8a1630d5654d096008c62717465a60e62d8dc
Yesus0212 <jesussoispadron@gmail.com>
2021-12-13 22:06:59 -0600

    Ejercicio fetch API Rick and Morty

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
+import RickandMortyCards from './components/RickandMortyCards';
+// import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <NavBar />       
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <NavBar />        */}
+        <h1>The Rick and Morty API</h1>
+        <RickandMortyCards />
       </header>
     </div>
   );
---
+++ my-app/src/components/RickandMortyCards/index.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from "react";
+import "./RickandMortyCards.css";
+
+function RickandMortyCards() {
+
+    const [cards, setCards] = useState([]);
+
+    const url = "https://rickandmortyapi.com/api/character";
+
+    useEffect(() => {        
+        const getInfoCards = async () => {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data.results);
+            setCards(data.results);
+        };
+
+        getInfoCards(); 
+    },[])
+
+    const _cards = cards.map((card) => (
+        <article key={card.id} className="characterCard">
+            <div className="imageCard">
+                <img src={card.image}/>
+            </div>
+            <div className="characterInfo">
+                <div>
+                    <a href={card.url}>
+                        <h2>{card.name}</h2>
+                    </a>
+                    <span>
+                        {card.species} {card.status}
+                    </span>
+                </div>                
+            </div>
+        </article>
+    ));
+    
+    return (
+        <>
+            {_cards}
+        </>
+    )
+}
+
+export default RickandMortyCards;
